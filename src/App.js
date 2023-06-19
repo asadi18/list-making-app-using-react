5e4fff56e57ba57707e98d61853b9fa1e3c0ad6e
asadi18 <asadishteaque@gmail.com>
2023-06-19 19:41:54 +0600

    This is a list making app of goal set using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+function Goalform(props) {
+  const [formData, setFormData] = React.useState({ goal: "", by: "" });
+
+  function changeHandle(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandle(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: "", by: "" });
+  }
+
+  return (
+    <>
+      <h1>My Diet Goals</h1>
+      <form onSubmit={submitHandle}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="goal"
+          value={formData.goal}
+          onChange={changeHandle}
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="By..."
+          value={formData.by}
+          onChange={changeHandle}
+        />
+        <button>Submit Data</button>
+      </form>
+    </>
+  );
+}
+
+function ListofGoal(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal}, by {g.by}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
+  const [allGoals, updateAllGoal] = React.useState([]);
+  function addGoal(goal) {
+    updateAllGoal([...allGoals, goal]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Goalform onAdd={addGoal} />
+      <ListofGoal allGoals ={allGoals}/>
     </div>
   );
 }
